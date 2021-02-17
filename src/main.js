c9a4399cbdbf1e45c74e3b7c1518bea9ecef9155
yurrezzz <kazanov.98@mail.ru>
2021-02-17 13:34:40 +0300

    Add store

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 
 const vue = new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App),
 });
 
